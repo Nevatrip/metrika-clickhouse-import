c22c19dd152b65554af262135e3843655f8d7a10
ploschka <folga12381@gmail.com>
2025-05-14 18:35:44 +1000

    Функция для доступа к переменным окружения

--- helpers/env.py
+++ helpers/env.py
@@ -1,6 +1,6 @@
 from dotenv import dotenv_values
 
-values = {
+__values: dict[str, str | None] = {
     'CLICKHOUSE_HOST': 'localhost',
     'CLICKHOUSE_USER': 'root',
     'CLICKHOUSE_PASSWORD': 'secret',
@@ -11,7 +11,7 @@ values = {
     'METRIKA_COUNTER': None,
     'METRIKA_KEY': None,
 
-    'METRIKA_CHAR_LIMIT': 3000,
+    'METRIKA_CHAR_LIMIT': '3000',
 
     'TEMP_HIT_TABLE_PREFIX': 'temp_hits',
     'HIT_TABLE_PREFIX': 'hits',
@@ -26,3 +26,14 @@ values = {
 
     **dotenv_values('.env', verbose=False),
 }
+
+def env_value_or_error(key: str) -> str:
+    val = __values[key]
+
+    if val is None:
+        raise Exception('UNDEFINED KEY')
+
+    if val.strip() == '':
+        raise Exception('UNDEFINED KEY')
+
+    return val
