5450ce85b408bb334bf16aa872358911e85028a2
ploschka <folga12381@gmail.com>
2025-06-10 15:57:42 +1000

    Файл для дебага инициализации

---
+++ fake_init.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import os
+from helpers.env import env_value_or_error
+import helpers.env as env
+import helpers.chosen_visit_params as cvp
+import helpers.chosen_hit_params as chp
+import helpers.funcs as funcs
+
+print(f"DROP DATABASE IF EXISTS {env_value_or_error(env.MAIN_DATABASE)}")
+print(f"DROP DATABASE IF EXISTS {env_value_or_error(env.TEMP_DATABASE)}")
+
+print(f"CREATE DATABASE {env_value_or_error(env.MAIN_DATABASE)}")
+print(f"CREATE DATABASE {env_value_or_error(env.TEMP_DATABASE)}")
+
+attributions = env_value_or_error(env.ATTRIBUTIONS).split()
+visit_key = tuple(env_value_or_error(env.TEMP_VISIT_KEY).split())
+hit_key = tuple(env_value_or_error(env.TEMP_HIT_KEY).split())
+
+visit_key = (visit_key[0], visit_key[1], visit_key[2])
+hit_key = (hit_key[0], hit_key[1], hit_key[2])
+
+visit_params = []
+hit_params = []
+
+for i1, i2 in funcs.divide_yandex_params(cvp.params, int(env_value_or_error(env.METRIKA_CHAR_LIMIT)), [visit_key]):
+    params = cvp.params[i1:i2]
+    params.append(visit_key)
+    visit_params.append(params)
+
+for i1, i2 in funcs.divide_yandex_params(chp.params, int(env_value_or_error(env.METRIKA_CHAR_LIMIT)), [hit_key]):
+    params = chp.params[i1:i2]
+    params.append(hit_key)
+    hit_params.append(params)
+
+temp_visit_prefix = f"{env_value_or_error(env.TEMP_DATABASE)}.{env_value_or_error(env.TEMP_VISIT_TABLE_PREFIX)}"
+for i, p in enumerate(visit_params):
+    for q in funcs.create_table_queries(temp_visit_prefix + str(i + 1), p, [visit_key[2]]):
+        print(q)
+
+temp_hit_prefix = f"{env_value_or_error(env.TEMP_DATABASE)}.{env_value_or_error(env.TEMP_HIT_TABLE_PREFIX)}"
+for i, p in enumerate(hit_params):
+    for q in funcs.create_table_queries(temp_hit_prefix + str(i + 1), p, [hit_key[2]]):
+        print(q)
+
+all_visit_params = cvp.params
+all_hit_params = chp.params
+
+all_visit_params.append(visit_key)
+all_hit_params.append(hit_key)
+
+main_visit_prefix = f"{env_value_or_error(env.MAIN_DATABASE)}.{env_value_or_error(env.VISIT_TABLE_PREFIX)}"
+main_hit_prefix = f"{env_value_or_error(env.MAIN_DATABASE)}.{env_value_or_error(env.HIT_TABLE_PREFIX)}"
+
+main_visit_keys = env_value_or_error(env.MAIN_VISIT_PRIMARY_KEYS).split()
+main_hit_keys = env_value_or_error(env.MAIN_HIT_PRIMATY_KEYS).split()
+
+for q in funcs.create_table_queries(main_visit_prefix, all_visit_params, main_visit_keys, attributions):
+    print(q)
+
+for q in funcs.create_table_queries(main_hit_prefix, all_hit_params, main_hit_keys, attributions):
+    print(q)
+
+day_count = int(env_value_or_error(env.DAY_COUNT))
+
+datefile = os.path.dirname(os.path.realpath(__file__)) + '/' + env_value_or_error(env.DATE_FILE)
+
+with open(datefile, 'w') as f:
+    f.write(funcs.get_init_dates(env_value_or_error(env.FIRST_DATE), day_count))
+
